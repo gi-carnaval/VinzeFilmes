93668699a29cd68a19f8cb53291cc8e7a4c8edbd
Giovani Carnaval <gi.carnaval91@gmail.com>
2023-02-28 23:38:42 -0300

    Update to extrabold on head title

--- src/components/FirstSection.tsx
+++ src/components/FirstSection.tsx
@@ -8,9 +8,8 @@ export function FirstSection() {
       <div className="flex flex-col-reverse items-center mt-24 px-5 justify-between lg:gap-16 lg:mb-24 lg:mx-[100px] md:flex-row">
         <section className="flex flex-col justify-center items-center lg:items-start lg:max-w-xl">
           <div className="flex flex-col-reverse justify-center items-center lg:block mt-24 lg:mt-0">
-            <h1 className="font-extrabold italic text-3xl text-center mt-6 lg:text-left lg:text-[68px] lg:leading-[64px]">
-              CONHEÇA A <span className="font-bold">VINZE</span>, E REVOLUCIONE
-              SEUS PROJETOS
+            <h1 className="font-black italic text-3xl text-center mt-6 lg:text-left lg:text-[68px] lg:leading-[64px]">
+              CONHEÇA A VINZE, E REVOLUCIONE SEUS PROJETOS
             </h1>
             <p className="text-[#959595] text-lg lg:text-base lg:mt-6">
               Invista em <span className="font-bold">QUALIDADE</span>
