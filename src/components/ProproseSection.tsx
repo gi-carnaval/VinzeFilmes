8b23db54e38071ad85ab2bf1df77359c8f1526be
Giovani Carnaval <gi.carnaval91@gmail.com>
2023-10-02 21:44:01 -0300

    Corrigindo bug no slide dos cards

--- src/components/ProproseSection.tsx
+++ src/components/ProproseSection.tsx
@@ -6,7 +6,7 @@ export function ProposeSection() {
   return (
     <div className="w-full flex flex-col justify-center items-center mt-24">
       <Title title={'Conheça o nosso propósito'} />
-      <div className="w-full my-32 px-[10%] flex flex-col items-center justify-center lg:mt-8 lg:flex-row lg:justify-between lg:items-stretch">
+      <div className="w-full my-32 px-[10%] flex flex-col items-center justify-center lg:mt-8 lg:flex-row lg:justify-between lg:items-stretch overflow-hidden">
         <CardGroup />
       </div>
     </div>
