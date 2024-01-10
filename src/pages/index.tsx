d30e683c7cc1e21f97580d591870f9a997f7839a
Giovani Carnaval <gi.carnaval91@gmail.com>
2024-01-10 13:39:00 -0300

    Feat [Loading] - Adding the loading component at home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,11 +2,13 @@ import { AboutSection } from '@/components/AboutSection'
 import { ClientsSection, LogoImages } from '@/components/ClientsSection'
 import { FirstSection } from '@/components/FirstSection'
 import { JobsSection, VideoProps } from '@/components/JobsSections'
+import { Loading } from '@/components/Loading'
 import { NumbersSection } from '@/components/NumbersSection'
 import { ProposeSection } from '@/components/ProproseSection'
 import { createClient } from '@/services/prismicio'
 import { GetServerSidePropsContext } from 'next'
 import Head from 'next/head'
+import { useEffect, useState } from 'react'
 
 interface HomeProps {
   logos: LogoImages[]
@@ -14,27 +16,41 @@ interface HomeProps {
 }
 
 export default function Home({ logos, videos }: HomeProps) {
+  const [isLoading, setIsLoading] = useState(true)
+  useEffect(() => {
+    // Wait for 3 seconds
+    setTimeout(() => {
+      setIsLoading(false)
+    }, 2000)
+  }, [])
+
   return (
     <>
-      <Head>
-        <title>Vinze Filmes</title>
-        <meta
-          property="og:image"
-          content="https://images.prismic.io/vinzefilmes/e1e4c231-9cc1-4e7d-ab6a-270806e0d2ab_ogImage.png?auto=compress,format"
-        />
-        <meta property="og:title" content="Vinze Filmes" />
-        <meta
-          name="description"
-          content="Conheça a Vinze Filmes e nossos projetos, e venha realizar sua produção conosco. "
-        ></meta>
-      </Head>
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <>
+          <Head>
+            <title>Vinze Filmes</title>
+            <meta
+              property="og:image"
+              content="https://images.prismic.io/vinzefilmes/e1e4c231-9cc1-4e7d-ab6a-270806e0d2ab_ogImage.png?auto=compress,format"
+            />
+            <meta property="og:title" content="Vinze Filmes" />
+            <meta
+              name="description"
+              content="Conheça a Vinze Filmes e nossos projetos, e venha realizar sua produção conosco. "
+            ></meta>
+          </Head>
 
-      <FirstSection />
-      <ProposeSection />
-      <AboutSection />
-      <ClientsSection logoImages={logos} />
-      <NumbersSection />
-      <JobsSection jobVideos={videos} />
+          <FirstSection />
+          <ProposeSection />
+          <AboutSection />
+          <ClientsSection logoImages={logos} />
+          <NumbersSection />
+          <JobsSection jobVideos={videos} />
+        </>
+      )}
     </>
   )
 }
